3c82ee8e6de1108b2ff783767bb6cbf18528b032
yhyuk <79249238+yhyuk@users.noreply.github.com>
2021-06-29 00:11:09 +0900

    Add files via upload

--- javascript/ex31.js
+++ javascript/ex31.js
@@ -1,8 +1,7 @@
-// ex31.jg
+// ex31.js
 
-// var num = 0;
-// -변수 선언문 반드시 작성 해야 한다.
-// npm install -g eslint -> cmd로 있어야함
+//ECMAScript
+// - 변수 선언문 반드시 작성해야 한다.
 
 var num = 10;
 console.log(num);
